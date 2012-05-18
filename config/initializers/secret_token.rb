044fb86a9bc4e7e012bcd2a4a46693a0fbb0cc51
work <work@bulletproof.(none)>
2012-05-17 20:41:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omg::Application.config.secret_token = 'fec78b7a0bffebf78927a423d099b5c5b1611862ba163c2ec6ebbd5b8fc0ea698407a561cb3bab17c78c5c3f02b51b2214c384062fd226f96a56f92219192914'
