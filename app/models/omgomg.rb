4a193715b86310af27afae98c2cefa88df913bd2
work <work@bulletproof.(none)>
2012-05-17 20:42:56 -0400

    add omgomgs

---
+++ app/models/omgomg.rb
@@ -0,0 +1,3 @@
+class Omgomg < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ db/migrate/20120518004233_create_omgomgs.rb
@@ -0,0 +1,8 @@
+class CreateOmgomgs < ActiveRecord::Migration
+  def change
+    create_table :omgomgs do |t|
+
+      t.timestamps
+    end
+  end
+end
